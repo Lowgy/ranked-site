8d33a22be5ab2915da18cbd5d97c8a7196e68a26
Logan Olson <loganolsonn@gmail.com>
2024-02-18 11:06:19 -0600

    Fixed achievements to display season and playoff that are displayed on the users profile

--- components/achievement-badge.tsx
+++ components/achievement-badge.tsx
@@ -7,72 +7,42 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 
-function extractWordByIndex(inputString: string, index: number) {
-  let words = inputString
-    .replace(/\b\w/g, (match) => match.toUpperCase())
-    .split(/\s+/) // Split the string by whitespace characters
-  return words[index] || "" // Return the word at the given index or an empty string if the index is out of bounds
-}
-
-function extractSeasonNumber(inputString: string) {
-  if (typeof inputString !== "string") {
-    throw new Error("Input must be a string.")
-  }
-
-  let capitalize = inputString.replace(/\b\w/g, (match) => match.toUpperCase())
-  return capitalize.slice(0, 8)
-}
-
-function extractSeasonPlacement(inputString: string) {
-  if (typeof inputString !== "string") {
-    throw new Error("Input must be a string.")
-  }
-  let test = extractWordByIndex(inputString, 2)
-  return test.slice(0, 3) + " " + test.slice(3)
-}
-
 export default function AchievementBadge(data: any) {
-  let formattedSentence = data.data.tag_name.replace(/_/g, " ")
-  let season = extractSeasonNumber(formattedSentence)
-  let placement = extractSeasonPlacement(formattedSentence).slice(4)
   return (
     <>
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger>
             <div className="relative">
-              <Image
-                src={
-                  placement === "1"
-                    ? "/sword.png"
-                    : placement === "2"
-                    ? "/goldsword.png"
-                    : placement === "3"
-                    ? "/ironsword.png"
-                    : placement === "5"
-                    ? "/diamond.png"
-                    : placement === "10"
-                    ? "/emerald.png"
-                    : placement === "50"
-                    ? "/redstone.png"
-                    : placement === "100"
-                    ? "/lapis.png"
-                    : "/coal.png"
-                }
-                alt="Badge"
-                height={100}
-                width={100}
-                className="mr-2"
-              />
+              {data.data.id === "playoffsResult" ? (
+                <Image
+                  src={`/icons/playoffs_${data.data.level}.png`}
+                  alt="Badge"
+                  height={30}
+                  width={30}
+                  className="mr-2"
+                />
+              ) : (
+                <Image
+                  src={`/icons/season_${data.data.level}.png`}
+                  alt="Badge"
+                  height={30}
+                  width={30}
+                  quality={100}
+                  className="mr-2"
+                />
+              )}
             </div>
           </TooltipTrigger>
           <TooltipContent>
             <div className="text-center">
-              {season}
+              {data.data.id === "seasonResult"
+                ? `Season ${data.data.data[0]}`
+                : `Season ${data.data.data[0]} Playoffs`}
               <br />
-              {placement === "1" || placement === "2" || placement === "3"
-                ? `#${placement}`
-                : `Top ${placement}`}
+              {data.data.id === "seasonResult"
+                ? `Rank #${data.data.data[1]}`
+                : `Rank #${data.data.level}`}
             </div>
           </TooltipContent>
         </Tooltip>
