14c310c8e313dd78903738f8787e7ca31af810a0
Logan Olson <loganolsonn@gmail.com>
2024-03-16 13:25:17 -0500

    Added Phase points leaderboard

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,10 +1,18 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-import { Runner, eloColumns, timeColumns } from "./columns"
+import { Runner, eloColumns, phasePointsColumns, timeColumns } from "./columns"
 import { DataTable } from "./data-table"
 
 async function getEloLeaderboard() {
   const response = await fetch("https://mcsrranked.com/api/leaderboard", {
+    cache: "no-cache",
+  })
+  const data = await response.json()
+  return data.data.users as Runner[]
+}
+
+async function getPhasePointsLeaderboard() {
+  const response = await fetch("https://mcsrranked.com/api/phase-leaderboard", {
     cache: "no-cache",
   })
   const data = await response.json()
@@ -33,34 +41,49 @@ async function getTimeLeaderboard() {
 export default async function LeaderboardPage() {
   const eloData = await getEloLeaderboard()
   const timeData = await getTimeLeaderboard()
+  const phasePointsData = await getPhasePointsLeaderboard()
 
   return (
     <section className="container grid items-center pb-8 pt-6 md:py-10">
       <h1 className="text-2xl">Leaderboard</h1>
       <Tabs defaultValue="elo">
         <div className="relative">
-          <TabsList className="absolute right-0 top-12 hidden w-[200px] grid-cols-2 md:grid md:w-[300px]">
+          <TabsList className="absolute right-0 top-12 hidden w-[200px] grid-cols-3 md:grid md:w-[300px]">
             <TabsTrigger value="elo">Elo</TabsTrigger>
             <TabsTrigger value="time">Time</TabsTrigger>
+            <TabsTrigger value="phasePoints">Phase</TabsTrigger>
           </TabsList>
         </div>
         <TabsContent value="elo">
           <span className="text-gray-500 ">
             Current top 150 ranked player this season.
           </span>
-          <TabsList className="mt-2 grid w-full grid-cols-2 md:hidden md:w-[300px]">
+          <TabsList className="mt-2 grid w-full grid-cols-3 md:hidden md:w-[300px]">
             <TabsTrigger value="elo">Elo</TabsTrigger>
             <TabsTrigger value="time">Time</TabsTrigger>
+            <TabsTrigger value="phasePoints">Phase</TabsTrigger>
           </TabsList>
           <DataTable columns={eloColumns} data={eloData} />
         </TabsContent>
         <TabsContent value="time">
-          <span className="text-gray-500">Top run times for this season</span>
-          <TabsList className="mt-2 grid w-full grid-cols-2 md:hidden md:w-[300px]">
+          <span className="text-gray-500">Top run times for MCSRanked</span>
+          <TabsList className="mt-2 grid w-full grid-cols-3 md:hidden md:w-[300px]">
             <TabsTrigger value="elo">Elo</TabsTrigger>
             <TabsTrigger value="time">Time</TabsTrigger>
+            <TabsTrigger value="phasePoints">Phase</TabsTrigger>
           </TabsList>
           <DataTable columns={timeColumns} data={timeData} />
+        </TabsContent>
+        <TabsContent value="phasePoints">
+          <span className="text-gray-500">
+            Top 100 players with the most phase points this season.
+          </span>
+          <TabsList className="mt-2 grid w-full grid-cols-3 md:hidden md:w-[300px]">
+            <TabsTrigger value="elo">Elo</TabsTrigger>
+            <TabsTrigger value="time">Time</TabsTrigger>
+            <TabsTrigger value="phasePoints">Phase</TabsTrigger>
+          </TabsList>
+          <DataTable columns={phasePointsColumns} data={phasePointsData} />
         </TabsContent>
       </Tabs>
     </section>
